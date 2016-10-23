cda842dbf3e40aa419b460d0edb5a38e122717ba
王晗 <2367188553@qq.com>
2016-10-23 23:38:46 +0800

    Frame

--- NonlinearTransformLayerGradient.m
+++ NonlinearTransformLayerGradient.m
@@ -3,7 +3,7 @@
 %  ADMM-NET
 %
 %  Created by Wang Han.SCU on 22/10/16.
-%  Copyright (C) 2016 robotcloud. SCU. All rights reserved.
+%  Copyright (C) 2016 Deep ADMM NETWORK. SCU. All rights reserved.
 
 function g = NonlinearTransformLayerGradient(z)
 
