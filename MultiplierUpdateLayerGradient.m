92c8bc2a0419b42eea2e7980628234e646468746
王晗 <2367188553@qq.com>
2016-10-25 11:06:27 +0800

    update

--- MultiplierUpdateLayerGradient.m
+++ MultiplierUpdateLayerGradient.m
@@ -5,6 +5,15 @@
 %  Created by Wang Han.SCU on 22/10/16.
 %  Copyright (C) 2016 Deep ADMM NETWORK. SCU. All rights reserved.
 
-function g = MultiplierUpdateLayerGradient(z)
+function [beta2eta_gradient,beta2beta_gradient,...
+    beta2c_gradient,beta2z_gradient] = MultiplierUpdateLayerGradient(...)
 
+    % unfinish
+
+    % beta2eta_gradient is the gradient between beta(n) and eta(n)
+    % beta2beta_gradient is the gradient between beta(n) and beta(n-1)
+    % beta2c_gradient is the gradient between beta(n) and c(n)
+    % beta2z_gradient is the gradient between beat(n) and z(n)
+
+    
 end
