3458fea2c72a00dd48779b7d550ce65d908441a7
王晗 <2367188553@qq.com>
2016-11-15 10:17:03 +0800

    增加读取数据文件

---
+++ LoadImage.m
@@ -0,0 +1,14 @@
+%
+%  LoadImage.m
+%  ADMM-NET
+%
+%  Created by Wang Han.SCU on 15/11/16.
+%  Copyright (C) 2016 Deep ADMM NETWORK. SCU. All rights reserved.
+%
+clear,clc;
+img = imread('image2/IM-0001-0001.jpg');
+img = rgb2gray(img);
+img_k_space = real(fft2(img));
+
+net = Stage(3);
+net.reconstructionLaryer(1) = {img_k_space};
